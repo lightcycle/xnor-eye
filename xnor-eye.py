6a628efc0efebbf95913fd06722405fdd1382efe
Mike Davis <mike@lightcycle.org>
2019-08-03 22:05:43 -0500

    Wrap in a Flask app.

--- xnor-eye.py
+++ xnor-eye.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
-
+from flask import Flask
+from flask import jsonify
+from flask_api import status
+from flask_compress import Compress
+from io import BytesIO
 import os
 import sys
+import base64
 
 if sys.version_info[0] < 3:
     sys.exit("This sample requires Python 3. Please install Python 3!")
@@ -37,54 +41,35 @@ camera_frame_rate = 15
 camera_brightness = 65
 camera_shutter_speed = 1500
 camera_video_stablization = True
-		 
-def main(args=None):
-    # Reconstruct the input resolution to include color channel
-    input_res = (input_resolution[0], input_resolution[1], 3)
-    SINGLE_FRAME_SIZE_RGB = input_res[0] * input_res[1] * input_res[2]
 
-    # Initialize the camera, set the resolution and framerate
-    try:
-        camera = picamera.PiCamera()
-    except picamera.exc.PiCameraMMALError:
-        print("\nPiCamera failed to open, do you have another task using it "
-              "in the background? Is your camera connected correctly?\n")
-        sys.exit("Connect your camera and kill other tasks using it to run "
-                 "this sample.")
+input_res = (input_resolution[0], input_resolution[1], 3)
+SINGLE_FRAME_SIZE_RGB = input_res[0] * input_res[1] * input_res[2]
 
-    # Initialize the buffer for picamera to hold the frame
-    # https://picamera.readthedocs.io/en/release-1.13/api_streams.html?highlight=PiCameraCircularIO
-    stream = picamera.PiCameraCircularIO(camera, size=SINGLE_FRAME_SIZE_RGB)
-    # All essential camera settings
-    camera.resolution = input_res[0:2]
-    camera.framerate = camera_frame_rate
-    camera.brightness = camera_brightness
-    camera.shutter_speed = camera_shutter_speed
-    camera.video_stabilization = camera_video_stablization
+try:
+	camera = picamera.PiCamera()
+except picamera.exc.PiCameraMMALError:
+	print("\nPiCamera failed to open, do you have another task using it "
+		  "in the background? Is your camera connected correctly?\n")
+	sys.exit("Connect your camera and kill other tasks using it to run "
+			 "this sample.")
 
-    # Record to the internal CircularIO
-    camera.start_recording(stream, format="rgb")
-    # Load model
-    model = xnornet.Model.load_built_in()
+stream = picamera.PiCameraCircularIO(camera, size=SINGLE_FRAME_SIZE_RGB)
+camera.start_recording(stream, format="rgb")
+model = xnornet.Model.load_built_in()
 
-    while True:
-        # Get the frame from the CircularIO buffer.
-        cam_buffer = stream.getvalue()
-        # The camera has not written anything to the CircularIO yet
-        # Thus no frame is been captured
-        if len(cam_buffer) != SINGLE_FRAME_SIZE_RGB:
-            continue
-        # Passing corresponding RGB
-        model_input = xnornet.Input.rgb_image(input_res[0:2], cam_buffer)
-        # Evaluate
-        results = model.evaluate(model_input)
+app = Flask(__name__)
+Compress(app)
 
-        print(results);
+@app.route('/evaluate/')
+def evaluate():
+    cam_buffer = stream.getvalue()
+    if len(cam_buffer) != SINGLE_FRAME_SIZE_RGB:
+        return {'error': 'no camera data yet'}, status.HTTP_204_NO_CONTENT
+    model_input = xnornet.Input.rgb_image(input_res[0:2], cam_buffer)
+    results = model.evaluate(model_input)
 
-    print("Cleaning up...")
-    camera.stop_recording()
-    camera.close()
+    image = Image.frombytes("RGB", input_res[0:2], cam_buffer)
+    byte_io = BytesIO()
+    image.save(byte_io, 'JPEG', quality=70)
 
-
-if __name__ == "__main__":
-    main()
+    return jsonify({'results': results, 'image': base64.b64encode(byte_io.getvalue()).decode('ascii')})
